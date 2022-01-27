f6d452163cd4137896133af7b4c02f804078e7d6
Magnus-Cosmos <maximuscosmos@gmail.com>
2022-01-27 18:59:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="brparser",
-    version="1.0.3",
+    version="1.0.4",
     description="A simple beatmap and replay parser.",
     author="Magnus Cosmos",
     url="https://github.com/Magnus-Cosmos/simpleparser",
